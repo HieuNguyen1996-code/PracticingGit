de554d867069b05a74f48c6753967f8e9a0b31ef
HieuNguyen1996-code <53060767+HieuNguyen1996-code@users.noreply.github.com>
2020-06-19 18:22:41 -0500

    Update HelloGit.java

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -1,6 +1,6 @@
 
 public class HelloGit {
-
+//change some code from git
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Pushing new file to github");
